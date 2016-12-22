945c2951a522202c352b7c47a89007a7c0c8b607
Markus Englund <markus.s.englund@gmail.com>
2016-12-22 21:41:25 +0100

    final adjustments

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express"),
       app = express(),
       multer = require("multer"),
+      upload = multer(),
       path = require("path"),
       port = (process.env.PORT || 3000)
 
@@ -8,7 +9,15 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
+app.post("/upload", upload.single("myFile"), (req, res) => {
+    if (req.file) {
+        res.json({ size: req.file.size });
+    } else {
+        res.json({ error: "File not found" })
+    }
+});
 
 
 
-app.listen(port, () => console.log("Sever is listening on port " + port))
+
+app.listen(port, () => console.log("Sever is listening on port " + port));
